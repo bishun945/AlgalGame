beace0d3051b60b2df0d5c2101f7fa927a1a3c6d
bishun945 <cookie_bis@qq.com>
2021-04-14 14:15:16 +0800

    Update AlgalGameShiny.R
    use `Reactive` functions in more appropriate way and add a download button for simulation data

--- inst/shiny-examples/AlgalGameShiny.R
+++ inst/shiny-examples/AlgalGameShiny.R
@@ -18,44 +18,77 @@ parms_input <- lapply(1:length(parms_name), makelist, obj = parms_name, width = 
 # parms_grid  <- lapply(1:length(grid_name), makelist, obj = grid_name, width = 100)
 
 server <- function(input, output) {
+
+  # running
+
   pp <- eventReactive(input$run, {
-      # algal running function starts from here
-      # qplot(input$Rin, input$h)
-      run_model(
-        Ttot = input$Ttot,
-        dt = input$dt,
-        L = input$L,
-        N = input$N,
-        inputs = as.list(input)
-      )$gplot
+    # algal running function starts from here
+    # qplot(input$Rin, input$h)
+    inputs <- reactiveValuesToList(input)[parms_list$Param]
+    run_model(
+      Ttot = input$Ttot,
+      dt = input$dt,
+      L = input$L,
+      N = input$N,
+      inputs = inputs
+    )
   })
 
-  output$AG <- renderPlot({
-    pp()
+  dt <- eventReactive(input$run, {
+    as.data.frame(as.matrix(reactiveValuesToList(input)[-1]))
   })
+
+  # output
+
+  output$AG <- renderPlot({
+    (pp())$gplot
+  })
+
+  output$param_df <- DT::renderDT(
+    dt()
+  )
+
+  output$download <-
+    downloadHandler("AlgalGameSimulation.csv", content = function(file) {
+      write.csv((pp())$out, file)
+    })
 
 }
 
 
 ui <- fluidPage(
+
   headerPanel("Algal Game"),
+
   sidebarLayout(
+
     sidebarPanel(
-      h4("Click `Run model` once all parameters are set"),
-      actionButton("run", "Run model"),
+
+      h4("Click `Run` once all parameters are set"),
+      actionButton("run", "Run", icon = icon("caret-right")),
+      downloadButton("download", "Download Simulation Data"),
+
       h4("Grid setting"),
       numericInput("Ttot", "Ttot", 2, width = 100),
       numericInput("dt", "dt", 1e-3, width = 100),
       numericInput("L", "L", 3, width = 100),
       numericInput("N", "N", 60, width = 100),
+
       h4("Parameters"),
-      lapply(parms_input, function(x) do.call("numericInput", x))
+      lapply(parms_input, function(x) do.call("numericInput", x)),
+
+      DT::DTOutput("param_df")
     ),
+
     mainPanel(
+
       h3("Simulation results"),
       plotOutput("AG")
+
     )
+
   )
+
 )
 
 
